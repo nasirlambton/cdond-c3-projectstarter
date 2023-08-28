a2235ac43e266b050e827a590530ae3441d8a1cb
nasirlambton <nasirlambton@gmail.com>
2023-08-28 06:52:50 -0400

    fix migration

--- .circleci/ansible/roles/configure-server/files/myenv.sh
+++ .circleci/ansible/roles/configure-server/files/myenv.sh
@@ -1,6 +1,6 @@
 export TYPEORM_CONNECTION=postgres
 export TYPEORM_ENTITIES=./src/modules/domain/**/*.entity.ts
-export TYPEORM_HOST=database-1.ckbbrvvqttmy.us-east-1.rds.amazonaws.com
+export TYPEORM_HOST=database-2.ctupcjqkfsj5.us-east-1.rds.amazonaws.com
 export TYPEORM_PORT=5432
 export TYPEORM_USERNAME=postgres
 export TYPEORM_PASSWORD=postgres
